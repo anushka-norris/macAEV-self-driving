d7c755d028bf4f21b53ed5accc1d2a6d51daba38
anushka-norris <anushka@fastmail.com>
2025-04-08 14:17:20 -0400

    Fixed some syntax errors

--- virtual_sep.py
+++ virtual_sep.py
@@ -55,14 +55,14 @@ class GapBarrier:
 
     def preprocess_lidar(self):
     #we want an array that contains all the scan distances in a "pizza slide" in front of the car, with all the distances that are too small set to zero.      
-    fov = 70 #in degrees, this is how wide we are looking on either side of the base_link x
+    	fov = 70 #in degrees, this is how wide we are looking on either side of the base_link x
 	start_index = 2*(180 - fov)
 	end_index = start_index + 2*(2*fov) #actually I guess I'm not using this one
 	for i in range(4*fov): #will do this loop once for each index within the "pizza slice" wedge of space in front of car
 		pizza_slice[i] = data.ranges[start_index + i]
 		if pizza_slice[i] <= self.d_stop: #all distances that are too small will be set to 0
 			pizza_slice[i] = 0
-		else
+		else:
 			continue
     return pizza_slice
              
@@ -70,22 +70,22 @@ class GapBarrier:
 
     # Optional function to find the the maximum gap in fron the vehicle 
     def find_max_gap(self, proc_ranges):
-    max_subset = []
-    max_subset_indices = []
-    current_max_subset = []
-    current_max_subset_indices = []
+    	max_subset = []
+    	max_subset_indices = []
+    	current_max_subset = []
+    	current_max_subset_indices = []
 	for i in len(pizza_slice): #going through the pizza slice, which is a new array, with indices starting from 0
-        if pizza_slice[i] > 0:
-            current_max_subset.append(pizza_slice[i]) #the measurement gets added to the current max array
-            current_max_subset.append(i) #the index of the measurement in the pizza slice 
-        else:
-            if len(current_max_subset) > len(max_subset): #the subset you just counted is actually the longest
-                max_subset = current_max_subset
-                max_subset_indices = current_max_subset_indices
+        	if pizza_slice[i] > 0:
+            		current_max_subset.append(pizza_slice[i]) #the measurement gets added to the current max array
+            		current_max_subset.append(i) #the index of the measurement in the pizza slice 
+        	else:
+            		if len(current_max_subset) > len(max_subset): #the subset you just counted is actually the longest
+                		max_subset = current_max_subset
+                		max_subset_indices = current_max_subset_indices
     #now we have the array max_subset which has the distances, and max_subset_indices which has the indices of it starting counting from the lidar scan represented by start_index. 
-        max_gap_start_index = start_index + max_subset_indices[0]
-        max_gap_end_index = start_index + max_subset_indices[-1]
-        gap_indices = [max_gap_start_index, max_gap_end_index]
+        			max_gap_start_index = start_index + max_subset_indices[0]
+        			max_gap_end_index = start_index + max_subset_indices[-1]
+        			gap_indices = [max_gap_start_index, max_gap_end_index]
         return gap_indices
         
 
@@ -113,15 +113,15 @@ class GapBarrier:
     def lidar_callback(self, data):      
 
     # Pre-process LiDAR data as necessary
-    pizza_slice = preprocess_lidar() #this is an array containing lidar distance values in front of car
+    	pizza_slice = preprocess_lidar() #this is an array containing lidar distance values in front of car
 	#now we have an array of the distances in the FOV, and want to find the longest span of nonzeros in it
     #find_max_gap() takes in the array pizza_slice of distances, outputs the indices in the pizza slice of the max gap that then need to be added to start_index.
-    gap_indices_in_pizza = find_max_gap(pizza_slice)
+    	gap_indices_in_pizza = find_max_gap(pizza_slice)
     #now we want the actual lidar index corresponding to the indices in the pizza slice
-    gap_start = start_index + gap_indices_in_pizza[0]
-    gap_end = start_index + gap_indices_in_pizza[1]
+    	gap_start = start_index + gap_indices_in_pizza[0]
+    	gap_end = start_index + gap_indices_in_pizza[1]
     #then just need to find largest distance within that gap
-    best_direction_index = find_best_point(gap_start, gap_end)
+    	best_direction_index = find_best_point(gap_start, gap_end)
     #now we have the index of the lidar scan in which direction we want the car to go (and its angle is half the scan index.)
 
 
